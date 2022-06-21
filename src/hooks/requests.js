5f7572ae485612ed9590d4babc98ee1674f2a705
Abbracx <tankoraphael@gmail.com>
2022-06-21 23:54:44 +0100

    feat: Added a submit functionality

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -9,12 +9,30 @@ async function httpGetPlanets() {
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  // Load launches, sort by flight number, and return as JSON.'
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchedLaunches = await response.json()
+  return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber )
 }
 
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'post',
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(launch)
+    })
+  } catch(err) {
+    //return an object with an ok status of false incase of network failures and may server cors issues, instead of leaving the request hanging.
+    return {
+      ok: false, 
+    }
+  }
+  
 }
 
 async function httpAbortLaunch(id) {
